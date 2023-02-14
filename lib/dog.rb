a896cb9d59ed6b8a149415fc6128bbd693d8c731
MwitaBrian <brianmwita13@gmail.com>
2023-02-15 02:37:18 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+  def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
 
-end
+    def name
+      @this_dogs_name="Lassie"
+    end
+  end
+
+  lassie = Dog.new
+  lassie.name = "Lassie"
+
+  puts lassie.name
+
+
